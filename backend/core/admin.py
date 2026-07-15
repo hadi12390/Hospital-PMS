b8bfb0e21229e8d8483767c44f33e8030e8bff4d
= <=>
2026-07-15 11:19:45 +0300

    adding the models to the admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Doctor, Patient, Appointment
+from .models import Doctor, Patient, Appointment, MedicalHistory, MedicalDocument, Manager
 
-admin.site.register(Doctor)
-admin.site.register(Patient)
-admin.site.register(Appointment)
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MedicalHistory)
+class MedicalHistoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MedicalDocument)
+class MedicalDocumentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    pass

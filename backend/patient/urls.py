277775a61de4d8c7bb626bc79c865a8b86cd3be3
= <=>
2026-08-15 17:31:06 +0300

    Patient dashboard

--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileEditView
+from .views import ProfileEditView, PatientDashboard
 
 urlpatterns = [
-    path('profile/', ProfileEditView.as_view())
+    path('profile/', ProfileEditView.as_view()),
+    path('dashboard/', PatientDashboard.as_view())
 ]

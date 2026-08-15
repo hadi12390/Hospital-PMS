d42818cb6467bc2aa2c0b2b490c6151702d47721
= <=>
2026-08-15 18:19:09 +0300

    Successful completion of a test on the doctor's profile and patient's dashboard

--- backend/doctor/urls.py
+++ backend/doctor/urls.py
@@ -3,5 +3,5 @@ from .views import DoctorDashboard, DoctorProfileEditView
 
 urlpatterns = [
     path('dashboard/', DoctorDashboard.as_view()),
-    path('profile/', DoctorProfileEditView.as_view())
+    path('profile/', DoctorProfileEditView.as_view(), name="doctor=profile")
 ]
--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -3,5 +3,5 @@ from .views import PatientProfileEditView, PatientDashboard
 
 urlpatterns = [
     path('profile/', PatientProfileEditView.as_view()),
-    path('dashboard/', PatientDashboard.as_view())
+    path('dashboard/', PatientDashboard.as_view(), name="patient-dashboard")
 ]
